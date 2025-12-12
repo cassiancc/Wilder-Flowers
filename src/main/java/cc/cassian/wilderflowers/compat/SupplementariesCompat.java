741c3c4273feaf7a5833157e43d34eb2103fa8fd
Cassian Godsted <cassian@godsted.com>
2025-12-12 14:37:47 -0500

    drop old 1.19 code

--- src/main/java/cc/cassian/wilderflowers/compat/SupplementariesCompat.java
+++ src/main/java/cc/cassian/wilderflowers/compat/SupplementariesCompat.java
@@ -9,11 +9,10 @@ import cc.cassian.wilderflowers.registry.WilderFlowersBlocks;
 public class SupplementariesCompat {
     public static void register() {
         //? if <1.21.2 {
-        /*//? <1.21.5
-        /^FlowerPotHandler.registerCustomFlower(WilderFlowersBlocks.CHEERY_WILDFLOWERS.flowerBedItem(), WilderFlowers.locate("block/cheery_wildflowers_potted"));^/
+        /*FlowerPotHandler.CUSTOM_MODELS.add(WilderFlowers.locate("block/cheery_wildflowers_potted"));
+        FlowerPotHandler.registerCustomSimpleFlower(WilderFlowersBlocks.CHEERY_WILDFLOWERS.flowerBedItem(), WilderFlowers.locate("block/cheery_wildflowers_potted"));
         FlowerPotHandler.registerCustomFlower(WilderFlowersBlocks.CHEERY_WILDFLOWERS.flowerBedItem(), WilderFlowers.locate("block/cheery_wildflowers_potted"));
-        //? <1.20
-        /^FlowerPotHandler.registerCustomFlower(WilderFlowersBlocks.PLAYFUL_WILDFLOWERS.flowerBedItem(), WilderFlowers.locate("block/cheery_wildflowers_potted"));^/
+        FlowerPotHandler.registerCustomFlower(WilderFlowersBlocks.PLAYFUL_WILDFLOWERS.flowerBedItem(), WilderFlowers.locate("block/cheery_wildflowers_potted"));
         FlowerPotHandler.registerCustomFlower(WilderFlowersBlocks.HOPEFUL_WILDFLOWERS.flowerBedItem(), WilderFlowers.locate("block/playful_wildflowers_potted"));
         FlowerPotHandler.registerCustomFlower(WilderFlowersBlocks.MOODY_WILDFLOWERS.flowerBedItem(), WilderFlowers.locate("block/moody_wildflowers_potted"));
         FlowerPotHandler.registerCustomFlower(WilderFlowersBlocks.HOPEFUL_WILDFLOWERS.flowerBedItem(), WilderFlowers.locate("block/hopeful_wildflowers_potted"));
